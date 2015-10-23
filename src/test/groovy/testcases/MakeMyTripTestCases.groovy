2730cfa2d88c5f8b75116fb8ad68e0afd5dab823
komeershettyvinod <vinod.komeershetty@imaginea.com>
2015-10-23 18:32:50 +0530

    Updated Payments Page

--- src/test/groovy/testcases/MakeMyTripTestCases.groovy
+++ src/test/groovy/testcases/MakeMyTripTestCases.groovy
@@ -2,6 +2,7 @@ package testcases
 
 import geb.spock.GebReportingSpec
 import makemytrip.pages.MakeMyTripLaunchPage
+import makemytrip.pages.PaymentsPage
 import makemytrip.pages.ReviewPage
 import makemytrip.pages.SearchResultsPage
 import makemytrip.pages.TravellersPage
@@ -16,18 +17,19 @@ class MakeMyTripTestCases extends GebReportingSpec{
     def  "Login to makeMyTrip account with valid details"() {
         given:
         to MakeMyTripLaunchPage
+
         when:  "User is trying to login to the application with valid details"
         closeOfferDialog()
         clickOnLoginLink()
         loginToMakeMyTripPortal("zq@yopmail.com","second")
+
         then: "user should be successfully logged into the application"
         verifyLogin()
-//        where username "zq@yopmail.com"
-
-
     }
     def "Search and Book For One Way Trip Ticket using Guest Login"(){
+
         at MakeMyTripLaunchPage
+
         when: "User tries to search for a One way trip flight ticket"
         clickOnOneWayOption()
         passValuestoFromField("Bangalore, India (BLR)")
@@ -36,41 +38,58 @@ class MakeMyTripTestCases extends GebReportingSpec{
         addAdults(1)
         clickOnSearchFlightButton()
         at SearchResultsPage
+
         then: "Appropriate search results should be displayed"
+
         verifyFromAndToAddress "Bangalore", "Hyderabad"
         verifyDepartureDate(startingdate_tweek)
         verifyAdultCount("01")
         verifyDepartureSearchRecord("Bangalore")
         verifyArrivalSearchRecord("Hyderabad")
+
+        when: "User tries to see Itenerary Details"
         clickOnShowDetails()
         clickOnItenararyButton()
         String dept_time=getDepartureTime()
         String arrival_time=getArrivalTime()
         String duration_time=getDurationTime()
         String flight_type=getFlightType()
+
+        then: "Approproate details should be displayed  in Itenarary tab"
         verifyItenararyDetails dept_time, arrival_time, "BLR", "Bangalore", "HYD", "Hyderabad",duration_time,flight_type
-       when: "Clicked on Book Button"
+
+        when: "Clicked on Book Button"
         clickOnBookButton()
+
         at ReviewPage
-        then:"User is Navigated to Review Page"
+
+        then:"User is Navigated to Review Page and appropriate details are displayed in Review Page"
         verifyDepartureDateinReviewPage(dept_time,"BLR", "Bangalore")
         verifyArrivalDateinReviewPage(arrival_time,"HYD", "Hyderabad")
         verifyDurationDateinReviewPage(duration_time, flight_type)
         verifyDeptDateinReviewPage(startingdate_tweek)
+
+        and: "In Review Page, User enters all the details and navigates to Travellers Page"
         String grandTotal= getGrandTotalinReviewPage()
         passValuesTOEmailField "zq@yopmail.com"
         clickOnContinueButtoninReviewPage()
-        when: "Clicked on I Agree Button"
         clickOnIAgreeButton()
+
         at TravellersPage
-        then:"User is Navigated to Travellers Page"
+        then: "Verify details in Travellers Page"
+        verifyDetailsinTravellersPage("Bangalore","Hyderabad",duration_time,dept_time,arrival_time,grandTotal)
+
+        and:  "Provide required details in Travellers Page and navigates to Payment Page by clicking on 'Continue to Payment' button"
         passValuesToFirstName "test"
         passValuesToLastName("tester")
         passValuesToAgeField("32")
         passValuesToPhoneField("9885500590")
-        verifyDetailsinTravellersPage("Bangalore","Hyderabad",duration_time,dept_time,arrival_time,grandTotal)
-        when: "Clicked on Continue to Payment Button"
         clickOnContinueToPaymentButton()
+
+        at PaymentsPage
+
         then: "User is Navigated to Payments Page"
+
+        print "Successfull"
     }
 }
